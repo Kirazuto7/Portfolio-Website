99cc927d0efc67f8c8b7cf6ef2f497bee91428b5
Jordan Sukhnandan <kirazuto7@gmail.com>
2023-06-12 21:25:38 -0400

    fixed spacing issue

--- portfolio/src/Components/AppNavbar.js
+++ portfolio/src/Components/AppNavbar.js
@@ -5,9 +5,9 @@ function AppNavbar() {
     return(
         <section id="NavBar" className={(window.location.pathname === ("/") && isMobile() && isPortraitMode()) ? Styles.HomepageNavLinks : Styles.NavLinks}>
             <a href="/" className={(window.location.pathname === ("/") && isMobile() && isPortraitMode()) ? Styles.HomepageLink : Styles.Link}>Home</a>
-            <a href="/resume" className={Styles.Link}>Resume</a>
-            <a href="/projects" className={Styles.Link}>Projects</a>
-            <a href="mailto:jordansukhnyc@gmail.com" className={Styles.Link}>Contact</a>
+            <a href="/resume" className={(window.location.pathname === ("/") && isMobile() && isPortraitMode()) ? Styles.HomepageLink : Styles.Link}>Resume</a>
+            <a href="/projects" className={(window.location.pathname === ("/") && isMobile() && isPortraitMode()) ? Styles.HomepageLink : Styles.Link}>Projects</a>
+            <a href="mailto:jordansukhnyc@gmail.com" className={(window.location.pathname === ("/") && isMobile() && isPortraitMode()) ? Styles.HomepageLink : Styles.Link}>Contact</a>
         </section>
     )
 }
