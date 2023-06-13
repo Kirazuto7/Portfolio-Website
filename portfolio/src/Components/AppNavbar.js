213fff66cea5d942e8c61bca007b23e10403cee2
Jordan Sukhnandan <kirazuto7@gmail.com>
2023-06-12 21:19:57 -0400

    fixed nav links for other routes

--- portfolio/src/Components/AppNavbar.js
+++ portfolio/src/Components/AppNavbar.js
@@ -1,9 +1,10 @@
+import { isMobile, isPortraitMode } from '../Exports';
 import Styles from '../Styles/AppNavbar.module.css';
 
 function AppNavbar() {
     return(
-        <section id="NavBar" className={Styles.NavLinks}>
-            <a href="/" className={Styles.Link}>Home</a>
+        <section id="NavBar" className={(window.location.pathname === ("/") && isMobile() && isPortraitMode()) ? Styles.HomepageNavLinks : Styles.NavLinks}>
+            <a href="/" className={(window.location.pathname === ("/") && isMobile() && isPortraitMode()) ? Styles.HomepageLink : Styles.Link}>Home</a>
             <a href="/resume" className={Styles.Link}>Resume</a>
             <a href="/projects" className={Styles.Link}>Projects</a>
             <a href="mailto:jordansukhnyc@gmail.com" className={Styles.Link}>Contact</a>
